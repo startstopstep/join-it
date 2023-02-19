404626fe3f65e2dabfcf950f83417a593158d6ae
Yurii Merker <yura.merk11@gmail.com>
2023-02-20 00:03:34 +0200

    Add tests for User model.

--- users/models.py
+++ users/models.py
@@ -11,6 +11,10 @@ from django.utils.translation import gettext_lazy as _
 
 class UserManager(DjangoUserManager):
     def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("User must have an email address.")
+
+        email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
